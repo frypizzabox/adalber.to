f3a1c298867e74cd414142d9d0f29a56a63c83b4
Adalberto Kutuxidis <Adalberto Kutuxidis>
2016-09-02 12:56:55 -0400

    Change Gallery Post Style

--- app/gallery-post/gallery-post.provider.js
+++ app/gallery-post/gallery-post.provider.js
@@ -6,7 +6,7 @@ GalleryPostProvider = function() {
         title: 'Hello Word',
         date: '20/08/2016',
         content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida. Cras sagittis augue et justo ornare varius. Nam mattis tempor eleifend. Donec tincidunt, turpis non tristique maximus, est nunc finibus sem, sed rhoncus eros magna quis erat. Curabitur viverra turpis at eros congue, nec mollis justo rhoncus. Phasellus pharetra tincidunt mattis. Praesent elementum molestie orci at commodo. Sed facilisis mi justo, et luctus nibh egestas ac. Praesent quis accumsan arcu.</p>',
-        imgSrc: 'app/home/washedout.png',
+        imgSrc: 'app/gallery-post/washedout.png',
         imgName: 'Washed Out'
     };
 
