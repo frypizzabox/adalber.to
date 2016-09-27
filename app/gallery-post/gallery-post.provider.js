35be102c7998c3ac57fc95a08654b24f71f44269
Adalberto Kutuxidis <Adalberto Kutuxidis>
2016-09-27 08:03:28 -0400

    Change Relevant Link in Provider

--- app/gallery-post/gallery-post.provider.js
+++ app/gallery-post/gallery-post.provider.js
@@ -8,38 +8,83 @@ GalleryPostProvider = function() {
         content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida. Cras sagittis augue et justo ornare varius. Nam mattis tempor eleifend. Donec tincidunt, turpis non tristique maximus, est nunc finibus sem, sed rhoncus eros magna quis erat. Curabitur viverra turpis at eros congue, nec mollis justo rhoncus. Phasellus pharetra tincidunt mattis. Praesent elementum molestie orci at commodo. Sed facilisis mi justo, et luctus nibh egestas ac. Praesent quis accumsan arcu.</p>',
         imgSrc: 'app/gallery-post/washedout.png',
         imgName: 'Washed Out',
-        relevantLinks: [
+        relevantLinks:
+        [
             {
-                title: 'Hello Word 1',
-                date: '20/08/2016',
-                content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
-                imgSrc: 'app/gallery-post/washedout.png',
-                imgName: 'Washed Out',
-                link: 'http://google.com'
+                category: 'Projects',
+                linkList:
+                [
+                    {
+                        title: 'Hello Word 1',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 2',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 1',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 2',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    }
+                ]
             },
             {
-                title: 'Hello Word 2',
-                date: '20/08/2016',
-                content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
-                imgSrc: 'app/gallery-post/washedout.png',
-                imgName: 'Washed Out',
-                link: 'http://google.com'
-            },
-            {
-                title: 'Hello Word 1',
-                date: '20/08/2016',
-                content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
-                imgSrc: 'app/gallery-post/washedout.png',
-                imgName: 'Washed Out',
-                link: 'http://google.com'
-            },
-            {
-                title: 'Hello Word 2',
-                date: '20/08/2016',
-                content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
-                imgSrc: 'app/gallery-post/washedout.png',
-                imgName: 'Washed Out',
-                link: 'http://google.com'
+                category: 'External Content',
+                linkList:
+                [
+                    {
+                        title: 'Hello Word 1',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 2',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 1',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    },
+                    {
+                        title: 'Hello Word 2',
+                        date: '20/08/2016',
+                        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus tristique magna non gravida...</p>',
+                        imgSrc: 'app/gallery-post/washedout.png',
+                        imgName: 'Washed Out',
+                        link: 'http://google.com'
+                    }
+                ]
             }
         ]
     };
