3a06d58176214f44876815d0f159fe720c96a7c8
Adalberto Kutuxidis <Adalberto Kutuxidis>
2016-09-29 11:18:43 -0400

    Add json for topbar links

---
+++ app/content/content.topbar.link.list.js
@@ -0,0 +1,26 @@
+[
+    {
+        "title": "Home",
+        "name": "Home",
+        "href": "#!home",
+        "imgSrc": "app/content/img/octocat.png"
+    },
+    {
+        "title": "Projects",
+        "name": "Projects",
+        "href": "#!projects",
+        "imgSrc": "app/content/img/octocat.png"
+    },
+    {
+        "title": "About",
+        "name": "About",
+        "href": "#!about",
+        "imgSrc": "app/content/img/octocat.png"
+    },
+    {
+        "title": "Test",
+        "name": "Test",
+        "href": "#!test",
+        "imgSrc": "app/content/img/octocat.png"
+    }
+]
--- app/topbar/topbar.provider.js
+++ app/topbar/topbar.provider.js
@@ -2,34 +2,17 @@
 TopBarProvider = function() {
 
     var navList = [{}];
-
-    var linkList = [
-        {
-            title: 'Home',
-            name: 'home',
-            href: 'http://google.com',
-            imgSrc: 'app/topbar/octocat.png'
-        },
-        {
-            title: 'Projects',
-            name: 'projects',
-            href: 'http://google.com',
-            imgSrc: 'app/topbar/octocat.png'
-        },
-        {
-            title: 'About',
-            name: 'about',
-            href: 'http://google.com',
-            imgSrc: 'app/topbar/octocat.png'
-        }
-    ];
+    var linkList = [{}];
 
     return {
+
         $get: function($http) {
 
             $http.get('app/content/content.topbar.navigation.js')
-                 .success(function (data) {
+                .success(function (data) {
+
                     for (var i = 0; i < data.length; i++) {
+
                         var navOption = {
                             title: data[i].title,
                             name: data[i].name,
@@ -38,10 +21,29 @@ TopBarProvider = function() {
 
                         navList.push(navOption);
                     }
-                 })
-                 .error(function (data, status, headers, config) {
-                     //  Do some error handling here
-                 });
+                })
+                .error(function (data, status, headers, config) {
+                    // Do some error handling here
+                });
+
+            $http.get('app/content/content.topbar.link.list.js')
+                .success(function (data) {
+
+                    for (var i = 0; i < data.length; i++) {
+
+                        var linkOption = {
+                            title: data[i].title,
+                            name: data[i].name,
+                            href: data[i].href,
+                            imgSrc: data[i].imgSrc
+                        };
+
+                        linkList.push(linkOption);
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    // Do some error handling here
+                });
 
             return {
                 navList: navList,
